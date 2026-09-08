e9a368bb3824e26eb17da125dc9f880f35771cb0
Karo Mkrtchyan <mkrtchyan.karo@gmail.com>
2026-09-08 15:26:25 +0400

    First

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@ BOT_NAME="$(basename "$(pwd)")"
 pm2 delete "${BOT_NAME}"
 rm -rf dist/
 git pull origin main
-npm install
-cp .env.example .env
-cp users.config.example.json users.config.json
 npm run build
 pm2 start dist/app.js --name "${BOT_NAME}"
 pm2 save
